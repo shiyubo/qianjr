a642102d9db8463a791c02b240b88d63e7ebc8fb
Charlotte <Charlotte@SharlottedeMacBook-Pro.local>
2017-07-15 14:24:29 +0800

    motys

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import Vuex from 'vuex'
 import router from './router'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 
+Vue.use(Vuex)
 Vue.use(MintUI)
 
 /* eslint-disable no-new */
