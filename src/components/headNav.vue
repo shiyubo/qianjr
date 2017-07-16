feded3d86620c98b28521aac40fd8895cabe4dc8
Charlotte <Charlotte@SharlottedeMacBook-Pro.local>
2017-07-16 12:38:50 +0800

    motys

--- src/components/headNav.vue
+++ src/components/headNav.vue
@@ -1,18 +1,18 @@
 <template>
-  <mt-tabbar v-model="selected" fixed>
-    <mt-tab-item id="首页" @click.native="$router.push('/home')">
+  <mt-tabbar v-model="headNav" fixed>
+    <mt-tab-item id="tabbar1" @click.native="goRouter(1)">
       <img slot="icon" src="../assets/logo.png">
       首页
     </mt-tab-item>
-    <mt-tab-item id="理财" @click.native="$router.push('/productList')">
+    <mt-tab-item id="tabbar2" @click.native="goRouter(2)">
       <img slot="icon" src="../assets/logo.png">
       理财
     </mt-tab-item>
-    <mt-tab-item id="我的" @click.native="$router.push('/myAccount')">
+    <mt-tab-item id="tabbar3" @click.native="goRouter(3)">
       <img slot="icon" src="../assets/logo.png">
       我的
     </mt-tab-item>
-    <mt-tab-item id="更多" @click.native="$router.push('/more')">
+    <mt-tab-item id="tabbar4" @click.native="goRouter(4)">
       <img slot="icon" src="../assets/logo.png">
       更多
     </mt-tab-item>
@@ -20,14 +20,37 @@
 </template>
 
 <script type="es6">
-  import { Navbar, TabItem } from 'mint-ui';
+  import { Navbar, TabItem } from 'mint-ui'
+  import{ mapState } from 'vuex'
   export default {
-    data () {
+    /*data () {
       return {
         selected: '首页'
       }
+    },*/
+    components: { Navbar, TabItem },
+    computed: {
+      ...mapState(['headNav'])
     },
-    components: { Navbar, TabItem }
+    methods: {
+      goRouter(index){
+        this.$store.commit('setHeadNav', index);
+        switch (index) {
+          case 1:
+            this.$router.push({path: '/home'});
+            break;
+          case 2:
+            this.$router.push({path: '/product'});
+            break;
+          case 3:
+            this.$router.push({path: '/account'});
+            break;
+          case 4:
+            this.$router.push({path: '/more'});
+            break;
+        }
+      }
+    }
   }
 </script>
 
