27aef17b7e44b0c5d438ddde5693a0cbf6f4fcb0
Charlotte <Charlotte@SharlottedeMacBook-Pro.local>
2017-07-18 00:56:10 +0800

    motys

--- src/main.js
+++ src/main.js
@@ -11,9 +11,8 @@ Vue.use(MintUI)
 /* eslint-disable no-new */
 // 路由器会创建一个 App 实例，并且挂载到选择符 #app 匹配的元素上。
 new Vue({
-  el: '#app',
   router,
   store,
   template: '<App/>',
   components: { App }
-})
+}).$mount('#app')
